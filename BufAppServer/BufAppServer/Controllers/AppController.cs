baddc4a7ff036b1b7833c4ca2371d1d2699496b4
Sander Wiig <sander.wiig@student.uib.no>
2022-09-14 11:56:52 +0200

    Changed server to asp.net

---
+++ BufAppServer/BufAppServer/Controllers/AppController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BufAppServer.Controllers;
+
+[ApiController]
+public class AppController : ControllerBase
+{
+    private readonly ILogger<AppController> _logger;
+    public AppController(ILogger<AppController> logger)
+    {
+        _logger = logger;
+    }
+    
+}
--- Server/Server/IHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Server;
-
- /**
-  * 
-  */
-public interface IHandler
-{
-    
-}
